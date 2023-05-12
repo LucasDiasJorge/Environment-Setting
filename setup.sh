e246c01140f26d051a92754746c9946f8c471d6e
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-05-12 09:54:48 -0300

    Update PGAdmin installation
    https://www.pgadmin.org/download/pgadmin-4-apt/

--- setup.sh
+++ setup.sh
@@ -83,9 +83,8 @@ ls /opt/gradle/gradle-8.1.1
 wget https://github.com/balena-io/etcher/releases/download/v1.18.4/balenaEtcher-1.18.4-x64.AppImage
 
 # Instala o PGAdmin 4
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-sudo apt-get update
-sudo apt install pgadmin4 pgadmin4-apache2
+curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install pgadmin4-desktop
 
 echo "Configuração concluída!"
