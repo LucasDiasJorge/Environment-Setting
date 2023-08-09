f9e2d5c15b848746c0022bfa289f8a841772a5c8
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-08-08 22:55:20 -0300

    Adding openboard

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -43,4 +43,7 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo
 sudo dnf install code
 
+# Instalar whiteboard
+flatpak install flathub ch.openboard.OpenBoard
+
 echo "Instalação concluída com sucesso!"
