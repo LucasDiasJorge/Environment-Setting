4b6d1a8b621424cc5682c89feb55828d9d3122f8
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-10-30 10:12:22 -0300

    Update setup_fedora.sh

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -40,9 +40,6 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo
 sudo dnf install code
 
-# Instalar whiteboard
-flatpak install flathub ch.openboard.OpenBoard
-
 # Instalar Snap
 sudo dnf install snapd
 sudo ln -s /var/lib/snapd/snap /snap
